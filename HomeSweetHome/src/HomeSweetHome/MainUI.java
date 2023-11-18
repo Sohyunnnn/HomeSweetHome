5678138ee4e5c79b90610467b2532a313b576492
Sohyunnnn <thgus05061@naver.com>
2023-11-18 14:16:06 +0900

    feat: 시작화면 패널 추가

--- HomeSweetHome/src/HomeSweetHome/MainUI.java
+++ HomeSweetHome/src/HomeSweetHome/MainUI.java
@@ -6,24 +6,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainUI extends JFrame {
+	private JPanel startPanel;
+
+	
     public MainUI() {
         setTitle("Home Sweet Home");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(984, 662);
+        
+        startPanel = new JPanel();
+        startPanel.setLayout(null);
 
         JButton loginButton = new JButton("로그인");
         JButton signUpButton = new JButton("회원가입");
         
-        JTextField Logo= new JTextField("Home Sweet Home"); //로고 이미지 넣을 예정
+        JTextField logo= new JTextField("Home Sweet Home"); //로고 이미지 넣을 예정
 
         
         loginButton.setBounds(361, 349, 260, 59);
         signUpButton.setBounds(361, 458, 260, 59);
-        Logo.setBounds(43,139,898,130);
+        logo.setBounds(43,139,898,130);
 
-        add(loginButton);
-        add(signUpButton);
-        add(Logo);
+        startPanel.add(loginButton);
+        startPanel.add(signUpButton);
+        startPanel.add(logo);
 
 
         loginButton.addActionListener(new ActionListener() {
@@ -37,11 +43,11 @@ public class MainUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // 회원가입 버튼을 눌렀을 때 수행할 로직을 여기에 추가
+            	new SignUpPage();
             }
         });
         
-        setLayout(null);
-
+        add(startPanel);
         setVisible(true);
     }
 
